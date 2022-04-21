b9c43efa5f9707ddce5f3c64cae2d447edb8d11c
carlos chaucanez <cachaucanes23@gmail.com>
2022-04-21 16:58:53 -0500

    config branch to gh-pages

--- next.config.js
+++ next.config.js
@@ -15,8 +15,8 @@ module.exports = withPWA({
     path: '/',
     domains: ['placeimg.com', 'api.lorem.space'],
   },
-  //basePath: '/example-store-platzi-next', //To deploy gh-pages (nombre de repositorio)
-  //assetPrefix: '/example-store-platzi-next', //To deploy gh-pages (nombre de repositorio)
+  basePath: '/example-store-platzi-next', //To deploy gh-pages (nombre de repositorio)
+  assetPrefix: '/example-store-platzi-next', //To deploy gh-pages (nombre de repositorio)
   // env: {
   //   customKey: 'customValue'
   // },
